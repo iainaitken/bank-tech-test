a154ca6f13a4be41775d94289ee98343ec7cbe92
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-06 16:39:38 +0100

    rRefactored error checking to check for non-integers being input by user

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -12,17 +12,17 @@ class BankAccount
   end
 
   def deposit_funds(amount)
-    return 'Please enter a valid number.' if amount <= 0
     return 'Please enter an amount in pence; for example, for £10.50, enter 1050.' unless amount.is_a?(Integer)
+    return 'Please enter a valid number.' if amount <= 0
 
     @balance += amount
     "You have deposited #{format(amount)} into your account. Your balance is now #{format(@balance)}"
   end
 
   def withdraw_funds(amount)
-    return "You cannot withdraw that much; your account balance is #{format(@balance)}." if amount > @balance
     return 'Please enter an amount in pence; for example, for £10.50, enter 1050.' unless amount.is_a?(Integer)
-    
+    return "You cannot withdraw that much; your account balance is #{format(@balance)}." if amount > @balance
+
     @balance -= amount
     "You have withdrawn #{format(amount)} from your account."
   end
