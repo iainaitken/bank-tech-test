e6f703de0102d194b0d6409f8987c7455df07d86
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-07 10:26:14 +0100

    Add method to transaction class to calculate new balance after transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,9 +1,21 @@
 class Transaction
-  attr_reader :date, :type, :amount
+  attr_reader :date, :type, :amount, :balance
   
-  def initialize(date:, type:, amount:)
+  def initialize(date:, type:, amount:, starting_balance:)
     @date = date
     @type = type.to_sym
     @amount = amount.to_i
+    @balance = starting_balance.to_i
+    adjust_balance
+  end
+
+  private
+
+  def adjust_balance
+    if @type == :credit
+      return @balance += @amount
+    elsif @type == :debit
+      return @balance -= @amount
+    end
   end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,7 +1,12 @@
 require 'transaction'
 
 RSpec.describe Transaction do
-  subject { described_class.new(date: '06/04/2021', type: 'credit', amount: '500') }
+  subject { described_class.new(
+    date: '06/04/2021',
+    type: 'credit',
+    amount: '500',
+    starting_balance: '500'
+  ) }
 
   it 'stores the date of a transaction' do
     expect(subject.date).to eq('06/04/2021')
@@ -13,5 +18,9 @@ RSpec.describe Transaction do
 
   it 'stores the amount of the transaction' do
     expect(subject.amount).to eq(500)
+  end
+
+  it 'stores the updated balance of the account' do
+    expect(subject.balance).to eq(1000)
   end
 end
