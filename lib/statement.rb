e57ac843e51d7748affab03a33eb8e083771fcbc
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-07 10:30:09 +0100

    Update Statement class to hold a list of transactions

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,6 +1,12 @@
 class Statement
+  def initialize
+    @list = []
+  end
 
-
+  def list_transactions
+    @list
+  end
+  
   private
 
 
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,7 +1,15 @@
 require 'statement'
 
 RSpec.describe Statement do
-  subject { described_class }
+  subject { described_class.new }
 
+  it 'starts with an empty list of transactions' do
+    expect(subject.list_transactions).to eq([])
+  end
 
+  # describe '#add_transaction' do
+  #   it 'stores a list of transactions' do
+      
+  #   end
+  # end
 end
