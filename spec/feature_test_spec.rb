d2d76e38558ce731b0a47df7c17fa0825222ad28
iainaitken <iain_aitken@yahoo.co.uk>
2021-04-10 09:33:36 +0100

    Refactored feature test to use acceptance criteria

--- spec/feature_test_spec.rb
+++ spec/feature_test_spec.rb
@@ -5,17 +5,17 @@ require 'runfile'
 RSpec.describe 'feature test' do
   it 'user can use the program' do
     account = BankAccount.new
-    account.add_transaction(date: '01/01/2021', type: 'credit', amount: 2000)
-    account.add_transaction(date: '05/01/2021', type: 'debit', amount: 500)
-    account.add_transaction(date: '18/01/2021', type: 'debit', amount: 1000)
+    account.add_transaction(date: '10/01/2012', type: 'credit', amount: 1000)
+    account.add_transaction(date: '13/01/2012', type: 'credit', amount: 2000)
+    account.add_transaction(date: '14/01/2012', type: 'debit', amount: 500)
 
     expect { account.print_statement }
       .to output(/date || credit || debit || balance\n/).to_stdout
-      .and output(/|| || 1000 || 500/).to_stdout
-      .and output(/|| || 500 || 1500/).to_stdout
-      .and output(/|| 2000 || || 2000/).to_stdout
+      .and output(/|| || 500.00 || 2500.00/).to_stdout
+      .and output(/|| 2000.00 || || 3000.00/).to_stdout
+      .and output(/|| 1000.00 || || 1000.00/).to_stdout
 
-    expect(account.add_transaction(date: '27/01/2021', type: 'debit', amount: 1000))
+    expect(account.add_transaction(date: '27/01/2012', type: 'debit', amount: 3000))
       .to eq('You cannot withdraw that much; please try again.')
   end
 end
